a4f6cfee97744d95f775d4e4e4610eeed37d552f
jnp000 <jnp000@student.bham.ac.uk>
2022-12-01 20:48:55 +0000

    day one tidy up

--- DayOne.java
+++ DayOne.java
@@ -4,65 +4,52 @@ import java.io.BufferedReader;
 public class DayOne {
     
     final static String INPUT = "day-one-input.txt";
-
+    static int[] largestCalories = {0, 0, 0};
+    static int currentCalories = 0;
+    static int elfID = 0;
+    
     public static void main(String[] args) {
-        int[] largestCalories = {0, 0, 0};
-        int currentCalories = 0;
-        int elfID = 0;
-        String currentLine;
-
         try {
             FileReader fr = new FileReader(INPUT);
             BufferedReader br = new BufferedReader(fr);
-
+            String currentLine;
+            
             while ((currentLine = br.readLine()) != null) {
                 if (currentLine.equals("")) {
-                    System.out.println("Elf #" + String.valueOf(elfID++) + ": " + String.valueOf(currentCalories));
-                    
-                    if (currentCalories > largestCalories[0]) {
-                    	largestCalories[2] = largestCalories[1];
-                    	largestCalories[1] = largestCalories[0];
-                    	largestCalories[0] = currentCalories;
-                    	
-                    } else if (currentCalories > largestCalories[1]) {
-                    	largestCalories[2] = largestCalories[1];
-                    	largestCalories[1] = currentCalories;
-                    	
-                    } else if (currentCalories > largestCalories[2]) {
-                    	largestCalories[2] = currentCalories;
-                    }
-
+                    elfAnalysis();
                     currentCalories = 0;
                 } else {
                     currentCalories += Integer.parseInt(currentLine);
                 }
-
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
         
-        System.out.println("Elf #" + String.valueOf(elfID++) + ": " + String.valueOf(currentCalories));
-        
-        if (currentCalories > largestCalories[0]) {
-        	largestCalories[2] = largestCalories[1];
-        	largestCalories[1] = largestCalories[0];
-        	largestCalories[0] = currentCalories;
-        	
-        } else if (currentCalories > largestCalories[1]) {
-        	largestCalories[2] = largestCalories[1];
-        	largestCalories[1] = currentCalories;
-        	
-        } else if (currentCalories > largestCalories[2]) {
-        	largestCalories[2] = currentCalories;
-        }
+        elfAnalysis();
         
         System.out.println("Total Elves           : " + String.valueOf(elfID));
         System.out.println("1st Largest Calories  : " + String.valueOf(largestCalories[0]));
         System.out.println("2nd Largest Calories  : " + String.valueOf(largestCalories[1]));
         System.out.println("3rd Largest Calories  : " + String.valueOf(largestCalories[2]));
         System.out.println("Total Largest Calories: " + String.valueOf(largestCalories[0] + largestCalories[1] + largestCalories[2]));
+    }
+    
+    public static void elfAnalysis() {
+    	System.out.println("Elf #" + String.valueOf(elfID++) + ": " + String.valueOf(currentCalories));
+    	
+    	 if (currentCalories > largestCalories[0]) {
+         	largestCalories[2] = largestCalories[1];
+         	largestCalories[1] = largestCalories[0];
+         	largestCalories[0] = currentCalories;
+         	
+         } else if (currentCalories > largestCalories[1]) {
+         	largestCalories[2] = largestCalories[1];
+         	largestCalories[1] = currentCalories;
+         	
+         } else if (currentCalories > largestCalories[2]) {
+         	largestCalories[2] = currentCalories;
+         }
     }
 
 }
